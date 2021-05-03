4d2353985206051fc967d34f55a097aae399091a
reloadedd <reloadedd@protonmail.com>
2021-05-03 16:16:33 +0300

    Add boilerplate code for Spring Boot App

---
+++ src/main/java/com/reloadedd/correctanaddress/CorrectAnAddressApplication.java
@@ -0,0 +1,46 @@
+/*
+ *     "? Correct An Address" is a tool that will correct a partially correct address.
+ *     Copyright (C) 2021  Ionuț Roșca <ionut.rosca@info.uaic.ro>
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.reloadedd.correctanaddress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Application's starting point class which exposes the <code>/hello</code> route.
+ *
+ * @author  Ionuț Roșca
+ * @version 0.1.0
+ */
+@SpringBootApplication
+@RestController
+public class CorrectAnAddressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorrectAnAddressApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
---
+++ src/test/java/com/reloadedd/correctanaddress/CorrectAnAddressApplicationTests.java
@@ -0,0 +1,38 @@
+/*
+ *     "? Correct An Address" is a tool that will correct a partially correct address.
+ *     Copyright (C) 2021  Ionuț Roșca <ionut.rosca@info.uaic.ro>
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.reloadedd.correctanaddress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+/**
+ * Test class containing tests for the Application.
+ *
+ * @author  Ionuț Roșca
+ * @version 0.1.0
+ */
+@SpringBootTest
+class CorrectAnAddressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
